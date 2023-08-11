acef65ea905dabb5c2a4d7f6a1846a63ae7a0cba
YouMeBetween <930798500@qq.com>
2023-08-11 14:46:41 +0800

    删除一些不必要的代码

--- randomSelector/randomSelector.cpp
+++ randomSelector/randomSelector.cpp
@@ -5,18 +5,9 @@
 #include "includes/items.h"
 using namespace std;
 
-string choose(vector<string> *item);
-
 int main()
 {
     CItems items;
     string result = items.chooseOne();
     cout << result;
-}
-
-string choose(vector<string> *item) {
-    default_random_engine e;
-    uniform_int_distribution<int> u(0, item->size() - 1);
-    e.seed(time(0));
-    return item->at(u(e));
 }
