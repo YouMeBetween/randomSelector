ebf127ce886090eacca693e7de10e51f5529f74c
YouMeBetween <930798500@qq.com>
2023-08-22 11:03:44 +0800

    增大选项设置界面的界面宽度

--- randomSelector/src/itemsSetup.cpp
+++ randomSelector/src/itemsSetup.cpp
@@ -10,29 +10,29 @@ constexpr int ITEMS_PER_PAGE = 5;
 constexpr int OPTION_LINE = 3;
 constexpr int ITEMS_LINE = 5;
 constexpr int OPTION_LINE_MAX_COLUMN = 2;
-constexpr int OPTION_LINE_ARROW_OFFSET = 4;
-constexpr int OPTION_LINE_ARROW_INTERVAL = 8;
-constexpr int ITEMS_OFFSET = 2;
-constexpr int WEIGHT_OFFSET = 13;
-constexpr int MIN_WEIGHT_OFFSET = 20;
-constexpr int WAVY_LINE_OFFSET = 23;
-constexpr int MAX_WEIGHT_OFFSET = 26;
+constexpr int OPTION_LINE_ARROW_OFFSET = 6;
+constexpr int OPTION_LINE_ARROW_INTERVAL = 10;
+constexpr int ITEMS_OFFSET = 3;
+constexpr int WEIGHT_OFFSET = 20;
+constexpr int MIN_WEIGHT_OFFSET = 26;
+constexpr int WAVY_LINE_OFFSET = 29;
+constexpr int MAX_WEIGHT_OFFSET = 32;
 constexpr int THIS_OPTION_LINE_INDEX = 0;
 constexpr int THIS_BACK_COLUMN_INDEX = 2;
 
 void CItemsSetup::show()
 {
 	system("cls");
-	cout << "********************************\n";
-	cout << "*           选项设置           *\n";
-	cout << "********************************\n";
-	cout << "*   > 搜索    跳转    返回     *\n";
-	cout << "*   项目    权重    权重范围   *\n";
+	cout << "****************************************\n";
+	cout << "*               选项设置               *\n";
+	cout << "****************************************\n";
+	cout << "*     > 搜索      跳转      返回       *\n";
+	cout << "*     项目         权重   权重范围     *\n";
 	for (int i = 0; i != ITEMS_PER_PAGE; i++) {
-		cout << "*                              *\n";
+		cout << "*                                      *\n";
 	}
-	cout << "*      上一页      下一页      *\n";
-	cout << "********************************\n";
+	cout << "*         上一页        下一页         *\n";
+	cout << "****************************************\n";
 }
 
 void CItemsSetup::displayItems(int page)
@@ -48,7 +48,7 @@ void CItemsSetup::displayItems(int page)
 			cout << items.at(static_cast<unsigned long long>(page) * ITEMS_PER_PAGE + i).min_weight;
 		}
 		gotoxy(WAVY_LINE_OFFSET, ITEMS_LINE + i);
-		cout << "~";
+		cout << "~~";
 		if (items.at(static_cast<unsigned long long>(page) * ITEMS_PER_PAGE + i).max_weight != 0) {
 			gotoxy(MAX_WEIGHT_OFFSET, ITEMS_LINE + i);
 			cout << items.at(static_cast<unsigned long long>(page) * ITEMS_PER_PAGE + i).max_weight;
